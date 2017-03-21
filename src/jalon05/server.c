8bd38bb0b4641ab193cc1cc7d40190b4009b1f58
Loïc Franchi <loic.franchi.lf@gmail.com>
2017-03-21 18:56:15 +0100

    Update server.c

--- src/jalon05/server.c
+++ src/jalon05/server.c
@@ -208,125 +208,6 @@ int main(int argc, char** argv)
 
                             handle_msg_from_client( buffer,tab_user, tab_salon, i, &nb_connexion);
 
-
-/*
-                            //Deal with different cases when looking at the buffer
-                            // /who
-                            else if (strcmp(buffer, "/who\n") == 0){
-                                all_user = who(tab_user);
-                                if(write(i,all_user,strlen(all_user)) < 0){
-                                    error("erreur write");
-                                }
-                                memset(buffer, '\0', sizeof(buffer));
-
-                            }
-
-                            // --------------------- /whois ----------------------
-                            else if (strcmp(str_sub(buffer,0,6), "/whois ") == 0){
-
-                                pseudo=get_2_arg(buffer);
-
-                                for(j=0;j<nb_connexion;j++){
-
-                                    // Search the fitting user in the table
-                                    if (strcmp(pseudo,get_nickname(tab_user +j))==0){
-                                        //desc_user(tab_user+j);
-
-
-                                        whois_user(tab_user +j, pseudo, string_whois); //Fill the string with the right informations
-                                        if(write(i,string_whois,200) < 0){
-                                            error("erreur write");
-                                        }
-                                        memset(string_whois, '\0', sizeof(string_whois));
-                                        whois_indicator = 1;
-                                        break;
-                                    }
-                                }
-                                if (whois_indicator == 0){
-                                    if(write(i,"User not found in tab",50) < 0){
-                                        error("erreur write");
-                                    }
-                                }
-                                if (whois_indicator == 1){
-                                    whois_indicator=0;
-                                }
-
-
-                            }
-
-
-                            // /msgall
-                            else if (strcmp(str_sub(buffer,0,7), "/msgall ") == 0){
-
-                                get_msgall(buffer,msg_all);
-                                send_all(tab_user, msg_all, i-4);
-
-                                memset(msg_all, '\0', sizeof(msg_all));
-                                memset(buffer, '\0', sizeof(buffer));
-
-                            }
-                            // /unicast
-                            else if (strcmp(str_sub(buffer,0,4), "/msg ") == 0){
-
-                                unicast(tab_user,buffer, i-4);
-                                memset(buffer, '\0', sizeof(buffer));
-
-                            }
-
-                            // /create (salon)
-                            else if (strcmp(str_sub(buffer,0,7), "/create ") == 0){
-
-                                create_salon(get_2_arg(buffer), tab_user+i-4, tab_salon );
-                                memset(buffer, '\0', sizeof(buffer));
-
-                            }
-                            // /join (salon)
-                            else if (strcmp(str_sub(buffer,0,5), "/join ") == 0){
-
-                                join_salon(tab_user+i-4, tab_salon,get_2_arg(buffer));
-                                memset(buffer, '\0', sizeof(buffer));
-                            }
-
-                            // /quit (salon)
-                            else if(strcmp(str_sub(buffer,0,5), "/quit ") == 0){
-
-                                quit_salon(tab_salon, tab_user+i-4, get_2_arg(buffer));
-                                memset(buffer, '\0', sizeof(buffer));
-
-                            }
-                            // /send (user filename)
-                            else if(strcmp(str_sub(buffer,0,5), "/send ") == 0){
-                                extract_file(buffer, receiving_user, filename, 6);
-                                extract_addr_p2p(tab_user,receiving_user,dest_addr,dest_port);
-
-                                memset(p2p_msg, '\0', sizeof(p2p_msg));
-                                strcat(p2p_msg,"/connect ");
-                                strcat(p2p_msg,dest_addr);
-                                strcat(p2p_msg," ");
-                                strcat(p2p_msg,dest_port);
-
-
-                                if(write(i,p2p_msg,strlen(p2p_msg)) < 0){
-                                    error("erreur write");
-                                }
-
-                            }
-
-
-                            //write back to the client
-                            else{
-                                if(get_user_salon(tab_user+i-4) != NULL){
-                                    write_in_salon(tab_user,buffer,tab_user+i-4);
-
-                                }
-                                else{
-
-                                    if(write(i,msg_usage,strlen(msg_usage)) < 0){
-                                        error("erreur write");
-                                    }
-
-                                }
-                            }*/
                         }
 
                         // /quit
@@ -343,13 +224,8 @@ int main(int argc, char** argv)
                 }
 
 
-
-
             }
         }
-
-
-
 
         else{
             printf("erreur select = %d \n",_select);
